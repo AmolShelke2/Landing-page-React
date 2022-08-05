a91ba0c7b429c7067cc2ba43256858f95c50694f
Amol shelke <shelkeamol897@gmail.com>
2022-08-05 22:26:40 +0530

    Added State Data And created Anime Charecters component

---
+++ src/component/animes/characters.component.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import "./characters.styles.css";
+
+class Charecters extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      animes: [
+        {
+          title: "Demon Slayer",
+          imageUrl:
+            "https://m.media-amazon.com/images/M/MV5BZjZjNzI5MDctY2Y4YS00NmM4LTljMmItZTFkOTExNGI3ODRhXkEyXkFqcGdeQXVyNjc3MjQzNTI@._V1_.jpg",
+          id: 1,
+          linkUrl: "demonslayer",
+        },
+
+        {
+          title: "Tokyo Revengers",
+          imageUrl:
+            "https://www.xtrafondos.com/wallpapers/personajes-de-tokyo-revengers-8137.jpg",
+          id: 2,
+          linkUrl: "tokyorevengers",
+        },
+
+        {
+          title: "Jujutsu Kaisen",
+          imageUrl:
+            "https://m.media-amazon.com/images/M/MV5BNGY4MTg3NzgtMmFkZi00NTg5LWExMmEtMWI3YzI1ODdmMWQ1XkEyXkFqcGdeQXVyMjQwMDg0Ng@@._V1_.jpg",
+          id: 3,
+          linkUrl: "jujutsukaisen",
+        },
+
+        {
+          title: "Mushoko Tensei",
+          imageUrl:
+            "https://m.media-amazon.com/images/M/MV5BMGM2MzA5YzYtODc0Ni00ZjU4LWI4ZmUtZGJjNGU0ODY1MGJkXkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_.jpg",
+          id: 4,
+          linkUrl: "Mushoku Tensei",
+        },
+      ],
+    };
+  }
+}
+
+export default Charecters;
--- src/page/home/homepage.component.jsx
+++ src/page/home/homepage.component.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+// import Charecters from "../../component/animes/characters.component";
 
 import "./homepage.styles.css";
 
-const Homepage = () => (
+const Homepage = ({ charecters }) => (
   <div className="home-page">
     <h1>Hello World</h1>
   </div>
