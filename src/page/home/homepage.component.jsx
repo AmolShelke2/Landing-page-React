c807955ba89971ce3b39564bc80972980c316201
Amol shelke <shelkeamol897@gmail.com>
2022-08-05 16:00:08 +0530

    Creating anime category landing page single page project for practising react basics

---
+++ src/page/home/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./homepage.styles.css";
+
+const Homepage = () => (
+  <div className="home-page">
+    <h1>Hello World</h1>
+  </div>
+);
+
+export default Homepage;
